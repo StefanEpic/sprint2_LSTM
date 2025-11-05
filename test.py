6fe29e7af68781ae7d55f30d72bb2d8d27140334
Stefan <118810289+StefanEpic@users.noreply.github.com>
2025-11-05 17:35:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-from src.lstm_train import train_model
+from src.lstm_train import train_model, save_training_model, save_training_curves
 from src.lstm_model import LSTMModel
 from src.next_token_dataset import tokenizer, train_loader, val_loader
 
-
+# Создаем модель
 vocab_size = len(tokenizer)
 model = LSTMModel(vocab_size=vocab_size, hidden_dim=64, num_layers=3)
 model.tokenizer = tokenizer
 
+# Запускаем процесс обучения
+train_results = train_model(model, train_loader, val_loader, tokenizer, num_epochs=5, learning_rate=0.001)
 
-train_model(model, train_loader, val_loader, tokenizer, num_epochs=10, learning_rate=0.001)
+# Сохраняем итоговые результаты
+save_dir = save_training_model(train_results['model'])
+save_training_curves(train_results['train_losses'], train_results['val_rouge_scores'])
